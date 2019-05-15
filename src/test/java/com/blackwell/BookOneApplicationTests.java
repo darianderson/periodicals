b0748639e39fe73d403d8b0153d361c2e25ccbda
dmitryblackwell <dmitryblackwell@gmail.com>
2019-05-15 22:27:11 +0300

    Basic setup

---
+++ src/test/java/com/blackwell/BookOneApplicationTests.java
@@ -0,0 +1,20 @@
+package com.periodicals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+// Integration tests are really good thing.
+// Every time it helps you find an error, please increase this counter:
+// ErrorsFoundWithTest: 1
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookOneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
