248ac29b9d46cb5ce6237fee31ee72a4f2a5bc31
darianderson <giftdadys@gmail.com>
2019-05-25 20:10:06 +0300

    Add page constant and aop

---
+++ src/main/java/com/periodicals/constant/PageConstants.java
@@ -0,0 +1,7 @@
+package com.blackwell.constant;
+
+public class PageConstants {
+	public static final String REDIRECT_HOME = "redirect:/";
+	public static final String REDIRECT_BOOKS = "redirect:/book";
+	public static final String REDIRECT_USERS = "redirect:/users";
+}
